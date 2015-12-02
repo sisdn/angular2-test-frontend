0bce55cd962a524e77950319bc422e15ff494681
angular-cli <angular-cli@angular.io>
2015-12-02 11:35:40 +0300

    chore: initial commit from angular-cli v0.0.0

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/angular2';
+import {SisdnApp} from './app/sisdn';
+
+
+bootstrap(SisdnApp);
---
+++ src/app/sisdn.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'sisdn-app',
+  providers: [],
+  templateUrl: 'app/sisdn.html',
+  directives: [],
+  pipes: []
+})
+export class SisdnApp {
+
+  constructor() {}
+
+}
